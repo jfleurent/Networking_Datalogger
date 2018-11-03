c26bacfd25f41b1afd60688c441005c5b4bc5221
jeffrey <jeffreyfleurent@outlook.com>
2018-11-03 12:19:46 -0400

    Added package and directories

--- server-mongodb.js
+++ server-mongodb.js
@@ -1,8 +1,16 @@
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
+
 var MongoClient = require('mongodb').MongoClient;
-var url = "https://cloud.mongodb.com/v2/5bddc084c56c98220cd19c03#clusters/detail/Cluster0";
+var url = "mongodb://localhost:8080/Cluster0";
 
 MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
+  if (err) {console.log("Database Failed"); throw err;}
   console.log("Database created!");
   db.close();
 });
