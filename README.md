2cfff37049fd35d0305d13d52f0d307b1b038e11
jeffrey <jeffreyfleurent@outlook.com>
2018-11-24 13:46:40 -0500

    Deleted Old Code

--- README.md
+++
@@ -1,8 +0,0 @@
-# Networking Datalogger
-Project for networking class at Florida Gulf Coast University
-### npm commands
-* npm init
-* npm i mongodb@2.2.36
-* npm i express
-* npm i body-parser
-* npm i @angular/cli
--- web/src/app/material.ts
+++
@@ -1,8 +0,0 @@
-import {NgModule} from '@angular/core';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-
-@NgModule({
-  imports:[MatDatepickerModule],
-  exports:[MatDatepickerModule],
-})
-export class MaterialModule{}
