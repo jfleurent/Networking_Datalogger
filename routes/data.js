3d798540d9db61b73b98e1d8dc0963672bb4b08e
Edgar Meruvia Garron <emeruvia@gmail.com>
2018-11-26 06:54:20 -0500

    added basic mongoose schema

--- routes/data.js
+++ routes/data.js
@@ -1,8 +1,28 @@
-const express = require('express');
+
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/data', {useNewUrlParser: true})
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB...'));
+
+const dataLoggerSchema = new mongoose.Schema({
+    time: {type: Date, default: Date.now},
+    number: Number,
+    val: [ Number ]
+});
+
+const DataLogger = mongoose.model('DataLogger', dataLoggerSchema);
+
+async function logData() {
+    const dataLogger = new DataLogger({
+        number: 1,
+        val: [ 3 ]
+    });
+
+    const result = await dataLogger.save();
+    console.log(result);
+}
+
+logData();
 
 module.exports = mongoose;
